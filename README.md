330901fea15227731bbfbb1a9e960e9d80dc0fb3
Max Kueng <me@maxkueng.com>
2014-06-01 00:19:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+Last.fm Export Stream
+=====================
+
+A readable object stream of a Last.fm user's scrobble history.
+
+### Installation
+
+```bash
+npm install lastfmexportstream --save
+```
+
+### Options
+
+ - `apiKey`: (required) The Last.fm API key. If you don't have one,
+   [get one from Last.fm](http://www.last.fm/api/account/create).
+ - `user`: (required) The Last.fm username.
+ - `reverse`: (optional, default: false) By default, scrobbles are fetched
+   in chronological order, i.e. oldest scrobble first.
+   If `reverse: true` the scrobbles will be fetched in reverse order,
+   i.e. latest scrobble first.
+ - `tracksPerRequest`: (optional, default: 100, max: 200) The number of
+   tracks to be fetched per request.
+ - `requestsPerMinute`: (optional, default: 60) The maximum number of
+   Last.fm API requests per second.
+ - `errorDelay`: (optional, default: 10000) The number of milliseconds
+   to wait until the next request if an error occurs.
+
+### Track format
+
+ - `time`: A UTC unix timestamp in milliseconds. Represents the time of
+   the scrobble.
+ - `*MBID`: The [MusicBrainz](http://musicbrainz.org/) ID of the track,
+   artist or album respectively, if available.
+
+The rest is rather self explanatory.
+
+```javascript
+{ time: 1401571793000,
+  artist: 'The Dramatics',
+  title: 'Inky Dinky Wang Dang Doo',
+  album: 'The Golden Torch Revisited',
+  trackMBID: '2c769602-4dfe-458d-b5e6-b0895d120830',
+  artistMBID: '1af3791d-816f-4e36-b475-e40d29d1c25b',
+  albumMBID: '' }
+
+```
+
+### Example
+
+```javascript
+var through = require('through2');
+var RecentTracksStream = require('lastfmexportstream');
+
+var soma7soul = new RecentTracksStream({
+  apiKey: 'YOUR_LASTFM_API_KEY',
+  user: 'soma7soul'
+});
+
+soma7soul
+  .pipe(through.obj(function (track, enc, callback) {
+    console.log(track);
+    callback();
+  }));
+```
+
+
---
+++ index.js
@@ -0,0 +1,131 @@
+var util = require('util');
+var Readable = require('stream').Readable;
+var LastfmAPI = require('lastfmapi');
+var trickle = require('timetrickle');
+
+exports = module.exports = RecentTracksStream;
+
+function RecentTracksStream (options) {
+	if (!(this instanceof RecentTracksStream)) { return new RecentTracksStream(options); }
+	Readable.call(this, { objectMode: true });
+
+	options = options || {};
+
+	if (!options.user || !options.apiKey) {
+		return process.nextTick(function () {
+			this.emit('error', new Error('Please provide user and apiKey options'));
+		}.bind(this));
+	}
+
+	this.apiKey = options.apiKey;
+	this.user = options.user;
+	this.reverse = options.reverse || false;
+	this.tracksPerRequest = options.tracksPerRequest || 100;
+	this.requestsPerMinute = options.requestsPerMinute || 60;
+	this.errorDelay = options.errorDelay || 60000;
+
+	this.rateLimit = trickle(this.requestsPerMinute, 1000);
+
+	this.api = new LastfmAPI({
+		api_key: this.apiKey
+	});
+
+	this.totalPages = null;
+	this.currentPage = null;
+	this.trackBuffer = [];
+}
+
+util.inherits(RecentTracksStream, Readable);
+
+RecentTracksStream.prototype.transformTracks = function (tracks) {
+	if (!Array.isArray(tracks)) { tracks = [ tracks ]; }
+
+	return tracks.map(function (track) {
+		if (!track.date) {
+			var now = new Date();
+			var utc = new Date(now.getTime() + now.getTimezoneOffset() * 60000);
+			track.date = { uts: +utc };
+		} else {
+			track.date.uts = parseInt(track.date.uts, 10) * 1000;
+		}
+
+		return {
+			time: track.date.uts,
+			artist: track.artist['#text'],
+			title: track.name,
+			album: track.album['#text'],
+			trackMBID: track.mbid,
+			artistMBID: track.artist.mbid,
+			albumMBID: track.album.mbid
+		};
+	});
+};
+
+RecentTracksStream.prototype.getNext = function () {
+	if (!this.totalPages) {
+		this.rateLimit(function () {
+			this.api.user.getRecentTracks({
+				user: this.user,
+				tracksPerRequest: this.tracksPerRequest
+			}, function (err, response) {
+				if (err) {
+					return setTimeout(this.getNext.bind(this), 10000);
+				}
+
+				this.totalPages = parseInt(response['@attr'].totalPages, 10);
+				this.currentPage = this.reverse ? 1 : this.totalPages;
+
+				setTimeout(this.getNext.bind(this), 1000);
+			}.bind(this));
+		}.bind(this));
+
+		return;
+	}
+
+	var nextTrack = this.trackBuffer.shift();
+
+	if (nextTrack) {
+		return this.push(nextTrack);
+	}
+
+	if (this.reverse && this.currentPage >= this.totalPages) {
+		return this.push(null);
+	}
+
+	if (!this.reverse && this.currentPage < 1) {
+		return this.push(null);
+	}
+
+	this.rateLimit(function () {
+		this.api.user.getRecentTracks({
+			user: this.user,
+			tracksPerRequest: this.tracksPerRequest,
+			page: this.currentPage
+
+		}, function (err, response) {
+			if (err) {
+				return setTimeout(this.getNext.bind(this), 10000);
+			}
+
+			this.currentPage += (this.reverse) ? 1 : -1;
+
+			var tracks = this.transformTracks(response.track);
+
+			tracks.forEach(function (track) {
+				if (this.reverse) {
+					return this.trackBuffer.push(track);
+				}
+
+				this.trackBuffer.unshift(track);
+			}.bind(this));
+
+			process.nextTick(this.getNext.bind(this));
+		}.bind(this));
+	}.bind(this));
+
+
+};
+
+RecentTracksStream.prototype._read = function () {
+	this.getNext();
+};
